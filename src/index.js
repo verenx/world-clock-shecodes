53306c3cef32c68209f158807d33e140e8ce0470
verenx <155170760+verenx@users.noreply.github.com>
2024-02-27 11:07:37 +0100

    added select functionalities with momentjs

--- src/index.js
+++ src/index.js
@@ -1,38 +1,59 @@
 //Vienna
 function updateDateTime() {
   let viennaElement = document.querySelector("#vienna");
+  if (viennaElement) {
+    let viennaDateElement = viennaElement.querySelector(".date");
+    let viennaTimeElement = viennaElement.querySelector(".time");
 
-  let viennaDateElement = viennaElement.querySelector(".date");
-  let viennaTimeElement = viennaElement.querySelector(".time");
+    let viennaCurrentTz = moment().tz("Europe/Vienna");
 
-  let viennaCurrentTz = moment().tz("Europe/Vienna");
+    let viennaCurrentDate = viennaCurrentTz.format("MMMM Do YYYY");
+    let viennaCurrentTime = `${viennaCurrentTz.format(
+      "h:mm:ss"
+    )} <small>${viennaCurrentTz.format("A")}</small>`;
+    // format("h:mm:ss[<small>A[</small>]")
 
-  let viennaCurrentDate = viennaCurrentTz.format("MMMM Do YYYY");
-  let viennaCurrentTime = `${viennaCurrentTz.format(
-    "h:mm:ss"
-  )} <small>${viennaCurrentTz.format("A")}</small>`;
-  // format("h:mm:ss[<small>A[</small>]")
-
-  viennaDateElement.innerHTML = viennaCurrentDate;
-  viennaTimeElement.innerHTML = viennaCurrentTime;
+    viennaDateElement.innerHTML = viennaCurrentDate;
+    viennaTimeElement.innerHTML = viennaCurrentTime;
+  }
 
   //Perth
   let perthElement = document.querySelector("#perth");
+  if (perthElement) {
+    let perthDateElement = perthElement.querySelector(".date");
+    let perthTimeElement = perthElement.querySelector(".time");
 
-  let perthDateElement = perthElement.querySelector(".date");
-  let perthTimeElement = perthElement.querySelector(".time");
+    let perthCurrentTz = moment().tz("Australia/Perth");
 
-  let perthCurrentTz = moment().tz("Australia/Perth");
+    let perthCurrentDate = perthCurrentTz.format("MMMM Do YYYY");
+    let perthCurrentTime = `${perthCurrentTz.format(
+      "h:mm:ss"
+    )} <small>${perthCurrentTz.format("A")}</small>`;
+    // format("h:mm:ss[<small>A[</small>]")
 
-  let perthCurrentDate = perthCurrentTz.format("MMMM Do YYYY");
-  let perthCurrentTime = `${perthCurrentTz.format(
-    "h:mm:ss"
-  )} <small>${perthCurrentTz.format("A")}</small>`;
-  // format("h:mm:ss[<small>A[</small>]")
+    perthDateElement.innerHTML = perthCurrentDate;
+    perthTimeElement.innerHTML = perthCurrentTime;
+  }
+}
 
-  perthDateElement.innerHTML = perthCurrentDate;
-  perthTimeElement.innerHTML = perthCurrentTime;
+function displayNewCity(event) {
+  let newCity = event.target.value;
+  let cityName = newCity.split("/")[1].replace("_", " ").replace("ome", "omé");
+  let newCityTz = moment().tz(newCity);
+  let newCityElement = document.querySelector("#cities-displayed");
+  newCityElement.innerHTML = `<div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${newCityTz.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${newCityTz.format(
+          "h:mm:ss"
+        )} <small>${newCityTz.format("A")}</small></div>
+      </div>`;
 }
 
 setInterval(updateDateTime, 1000);
 updateDateTime();
+
+let citiesSelectElement = document.querySelector("#cities");
+citiesSelectElement.addEventListener("change", displayNewCity);
